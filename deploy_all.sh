af25d4a1ae2949edebdfb37ba5cfcf0b7b510080
Eason <ddrizzt@gmail.com>
2019-01-02 15:43:31 +0800

    change initial script

--- deploy_all.sh
+++ deploy_all.sh
@@ -5,8 +5,9 @@
 : '
 
 rm -rf /mnt/*
-cd /mnt/ && curl -H "Authorization: token e280bf479d351af3a96b887ecc764ca19bf4a165" -L https://github.com/GD-China-WeChat/wechat-oa/archive/master.zip > wechat-oa-master.zip
+cd /mnt/ && curl -H "Authorization: token e280bf479d351af3a96b887ecc764ca19bf4a165" -L https://github.com/ddrizzt/gdoawechat/archive/master.zip > wechat-oa-master.zip
 cd /mnt/ && unzip wechat-oa-master.zip
+mv /mnt/gdoawechat-master /mnt/wechat-oa-master
 cd /mnt/wechat-oa-master && chmod +x *.sh
 /mnt/wechat-oa-master/deploy_all.sh [dev]
 
